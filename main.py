6188360f8468273099b2b03014149c88bde07d27
derekSaga <40836760+derekSaga@users.noreply.github.com>
2020-01-22 09:15:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ n = ExemploSingleton()
 m = ExemploSingleton()
 
 """Resultado: criamos uma instancia da classe ExemploSingleton que funciona como ponto de acesso global."""
+"""Dessa forma toda classe instanciada ira acessar a mesma instancia."""
 """um bom exemplo de utilização desse padrão
  seria a utilização dele em um projeto que necessite de uma infraestrutura para controle do log de dados."""
-print(n == m)
+print(n == m) # Resultado: True
