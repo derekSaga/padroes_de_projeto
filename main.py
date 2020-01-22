3d6a84e4e0665bf3859b3081b22a5e42dc55a56a
derekSaga <derek.coelho@outlook.com.br>
2020-01-22 09:04:41 -0300

    Exemplo padrao singleton

---
+++ main.py
@@ -0,0 +1,17 @@
+from padroes.meta_singleton import MetaSingleton
+
+"""Classe utilizada como exemplo"""
+
+
+class ExemploSingleton(metaclass=MetaSingleton):
+    pass
+
+
+"""instancia de classes ExemploSingleton"""
+n = ExemploSingleton()
+m = ExemploSingleton()
+
+"""Resultado: criamos uma instancia da classe ExemploSingleton que funciona como ponto de acesso global."""
+"""um bom exemplo de utilização desse padrão
+ seria a utilização dele em um projeto que necessite de uma infraestrutura para controle do log de dados."""
+print(n == m)
--- padroes/meta_singleton.py
+++ padroes/meta_singleton.py
@@ -1,11 +1,11 @@
 """Neste exemplo implementarei o padrão Singleton como MetaClasse"""
 """Link para mais informações sobre metaclasses: https://pt.wikipedia.org/wiki/Metaclasse"""
 
-class Singleton(type):
+
+class MetaSingleton(type):
     _instance = {}
 
     def __call__(cls, *args, **kwargs):
-
         if cls not in cls._instance:
-            cls._instance[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instance[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
         return cls._instance[cls]
